ed87d88a0a93cea6affdffd4fa522853e22803f0
Abdullah <abdullahpoi@gmail.com>
2024-02-27 15:58:24 +0530

    Auth and login completed

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,33 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+//update user
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            }catch(error){
+                return res.status(500).json(error)
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body,
+            });
+            res.status(200).json("Account has been updated")
+        }catch(error){
+            return res.status(500).json(error);
+        }
 
-router.get("/",(req,res)=>{
-    res.send("hello its user router")
+    }else{
+        return res.status(403).json("You can update only your account")
+    }
 })
+//delete user
+//get a user
+//follow a user
+//unfollow a user
 
 module.exports= router
